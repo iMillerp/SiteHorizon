ab54bdf32281b255121393d9ba04847b46818d49
Miller Pereira <miller.98@hotmail.coom>
2013-07-01 16:49:58 -0300

    Upload  Teste arquivos ronaldo

---
+++ js/default.js
@@ -0,0 +1,71 @@
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id))
+        return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/pt_BR/all.js#xfbml=1&appId=154629538030586";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
+(function($) {
+    $(document).ready(function() {
+        $('#myCarousel').carousel();
+    });
+});
+
+(function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function() {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+ga('create', 'UA-41915337-1', 'radiohorizon.com.br');
+ga('send', 'pageview');
+
+$(document).ready(function() {
+    $(".box_skitter_large").skitter({
+        dots: true,
+        focus_position: "leftTop",
+        controls_position: "leftTop",
+        numbers_align: "center",
+        theme: "round",
+        labelAnimation: "slideUp",
+        show_randomly: true,
+        hideTools: true,
+        enable_navigation_keys: true,
+        progressbar: true,
+        focus: true,
+        controls: true
+    });
+});
+
+jQuery("document").ready(function($) {
+    var nav = $('#menu');
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            nav.addClass("navbar-fixed-top menu3");
+        } else {
+            nav.removeClass("navbar-fixed-top menu3");
+        }
+    });
+
+
+});
+
+jQuery("document").ready(function($) {
+    var nav = $('#menu2');
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            nav.addClass("menu4");
+        } else {
+            nav.removeClass("menu4");
+        }
+    });
+});
